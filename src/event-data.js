459647956722bf2ed3ceed5b793419028fc54d23
Thannatcha <152575189+Thannatcha@users.noreply.github.com>
2025-01-08 15:25:25 +0700

    Update event-data.js

--- src/event-data.js
+++ src/event-data.js
@@ -14,8 +14,8 @@ export function EventData2() {
     return(
         <div style={{textAlign:'center',margin:20}}>
             <button onClick={(ev)=> onClickStartStop(ev)}>Start</button><br/><br/>
-            <button onClick={(ev)=> onClickStartStop(ev)}>10+20</button><br/><br/>
-            <button onClick={(ev)=> onClickStartStop(ev)}>30*40</button><br/><br/>
+            <button onClick={(ev)=> onClickAdd(ev)}>10+20</button><br/><br/>
+            <button onClick={(ev)=> onClickAdd(ev)}>30*40</button><br/><br/>
         </div>
     )
 }
