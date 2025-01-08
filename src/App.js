fc52c19b4767c8ae93e37301588cb06a88de5331
Thannatcha <152575189+Thannatcha@users.noreply.github.com>
2025-01-08 14:14:33 +0700

    Problem of Class component
    Problem of Class component

--- src/App.js
+++ src/App.js
@@ -1,17 +1,9 @@
 import React, { Component } from 'react'
-import { Header , Content , Footer} from './func-components';
-import  Calendar from './class-component';
+import  Button from './class-component';
 
 
 function App() {
-  return (
-    <>
-    <Header/>
-    <p><center><Calendar/></center></p>
-    <Content/>
-    <Footer/>
-    </>
-  )
+  return <Button/>
 }
 
 export default App;
