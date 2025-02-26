fdbde11cf546c11461abf3ff13079948b67bd3c7
Thannatcha <152575189+Thannatcha@users.noreply.github.com>
2025-02-26 14:23:03 +0700

    commit form-text.js
    commit form-text.js

--- src/App.js
+++ src/App.js
@@ -1,12 +1,10 @@
 import React from 'react'
-import Router3 from './router3';
-
-
+import FormText from './form-text';
 
 function App() {
   
   return (
-      <Router3/>
+      <FormText/>
   )
  
 }
